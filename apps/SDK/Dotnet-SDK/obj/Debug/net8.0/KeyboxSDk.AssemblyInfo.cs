17f1224d742ab2f7721f09bc41a5df5dd92519f6
huzfm <huzaaifmushtaq@gmail.com>
2026-02-02 11:43:30 +0530

    feat: .NET sdk

--- apps/SDK/Dotnet-SDK/obj/Debug/net8.0/KeyboxSDk.AssemblyInfo.cs
+++ apps/SDK/Dotnet-SDK/obj/Debug/net8.0/KeyboxSDk.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("KeyBox license protection SDK for .NET apps")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c11ee61f6928df384b6992fc99ab4905c86bdd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+282f483fff25392f5b37c201fef865afe8585019")]
 [assembly: System.Reflection.AssemblyProductAttribute("KeyboxSDk")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KeyboxSDk")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
