5b88aa238e759a9ef431f0a66bda7c8500036aaf
huzfm <huzaaifmushtaq@gmail.com>
2026-03-06 14:55:40 +0530

    fix: fixed some critical bugs

--- apps/server/src/controllers/license.controller.ts
+++ apps/server/src/controllers/license.controller.ts
@@ -6,146 +6,142 @@ import { AuthRequest } from "./../middleware/jwt"
 import { invalidateCachedLicense } from "../cache/license.cache"
 
 interface LicenseBody {
-        productName: string
-        customer: string
-        duration: number
+     productName: string
+     customer: string
+     duration: number
 }
 
 export const createLicense = async (req: AuthRequest, res: Response) => {
-        try {
-                const { duration, clientId, projectId, services } = req.body
+     try {
+          const { duration, clientId, projectId, services } = req.body
 
-                if (!duration || duration < 1 || duration > 12)
-                        return res
-                                .status(400)
-                                .json({ message: "Invalid duration" })
+          if (!duration || duration < 1 || duration > 12)
+               return res.status(400).json({ message: "Invalid duration" })
 
-                if (!clientId || !projectId)
-                        return res
-                                .status(400)
-                                .json({ message: "Client & Project required" })
+          if (!clientId || !projectId)
+               return res
+                    .status(400)
+                    .json({ message: "Client & Project required" })
 
-                const issuedAt = new Date()
-                const expiresAt = new Date()
-                expiresAt.setMonth(issuedAt.getMonth() + duration)
+          const issuedAt = new Date()
+          const expiresAt = new Date()
+          expiresAt.setMonth(issuedAt.getMonth() + duration)
 
-                const key = generateKey(projectId)
+          const key = generateKey(projectId)
 
-                const license = await License.create({
-                        key,
-                        duration,
-                        issuedAt,
-                        expiresAt,
-                        status: Status.PENDING,
-                        services: services || ["Hosting"],
-                        user: req.userId!,
-                        client: clientId,
-                        project: projectId,
-                })
+          const license = await License.create({
+               key,
+               duration,
+               issuedAt,
+               expiresAt,
+               status: Status.PENDING,
+               services: services || ["Hosting"],
+               user: req.userId!,
+               client: clientId,
+               project: projectId,
+          })
 
-                res.status(201).json({
-                        message: "License created",
-                        license,
-                })
-        } catch (error) {
-                res.status(500).json({ error: (error as Error).message })
-        }
+          res.status(201).json({
+               message: "License created",
+               license,
+          })
+     } catch (error) {
+          console.error("[ERROR] context:", error) // full details on your server
+          res.status(500).json({ error: "Something went wrong." })
+     }
 }
 
 export const toggleLicense = async (req: Request, res: Response) => {
-        try {
-                const key = req.params.key
+     try {
+          const key = req.params.key
 
-                if (!key)
-                        return res
-                                .status(400)
-                                .json({ message: "License key is required" })
+          if (!key)
+               return res
+                    .status(400)
+                    .json({ message: "License key is required" })
 
-                const license = await License.findOne({ key })
-                if (!license)
-                        return res
-                                .status(404)
-                                .json({ message: "License not found" })
+          const license = await License.findOne({ key })
+          if (!license)
+               return res.status(404).json({ message: "License not found" })
 
-                license.status =
-                        license.status === Status.ACTIVE
-                                ? Status.REVOKED
-                                : Status.ACTIVE
+          if (license.status == Status.EXPIRED)
+               return res.status(400).json({
+                    message: "State of expired licenses cannot be changed",
+               })
 
-                await license.save()
+          license.status =
+               license.status === Status.ACTIVE ? Status.REVOKED : Status.ACTIVE
 
-                //  Invalidate cache because state CHANGED
-                await invalidateCachedLicense(key)
+          await license.save()
 
-                return res.json({
-                        message: `License status changed to ${license.status}`,
-                        key: license.key,
-                        status: license.status,
-                })
-        } catch (error: any) {
-                return res.status(500).json({
-                        message: "Failed to toggle status",
-                        error: error.message,
-                })
-        }
+          //  Invalidate cache because state CHANGED
+          await invalidateCachedLicense(key)
+
+          return res.json({
+               message: `License status changed to ${license.status}`,
+               key: license.key,
+               status: license.status,
+          })
+     } catch (error: any) {
+          return res.status(500).json({
+               message: "Failed to toggle status",
+               error: error.message,
+          })
+     }
 }
 
 export const getUsersAndLicenses = async (req: Request, res: Response) => {
-        try {
-                const users = await User.find().populate("licenses").exec()
-                return res.json({
-                        users,
-                })
-        } catch (error) {
-                return res.status(500).json({
-                        message: "Failed to retrieve users and licenses",
-                        error: (error as Error).message,
-                })
-        }
+     try {
+          const users = await User.find().populate("licenses").exec()
+          return res.json({
+               users,
+          })
+     } catch (error) {
+          return res.status(500).json({
+               message: "Failed to retrieve users and licenses",
+               error: (error as Error).message,
+          })
+     }
 }
 
 export const test = async (req: AuthRequest, res: Response) => {
-        try {
-                const user = await User.findById(req.userId).select("-password")
-                const licenses = await License.find({ user: req.userId })
+     try {
+          const user = await User.findById(req.userId).select("-password")
+          const licenses = await License.find({ user: req.userId })
 
-                return res.json({ user, licenses })
-        } catch (error) {
-                console.error(error)
-                res.status(500).json({ message: "Server Error" })
-        }
+          return res.json({ user, licenses })
+     } catch (error) {
+          console.error(error)
+          res.status(500).json({ message: "Server Error" })
+     }
 }
 
 export const getUserWithLicenses = async (req: AuthRequest, res: Response) => {
-        try {
-                const userId = req.params.id || req.userId
+     try {
+          const userId = req.params.id || req.userId
 
-                if (!userId) {
-                        return res
-                                .status(400)
-                                .json({ message: "User ID is required" })
-                }
+          if (!userId) {
+               return res.status(400).json({ message: "User ID is required" })
+          }
 
-                const user = await User.findById(userId)
-                        .select("-password_hash")
-                        .populate("licenses")
+          const user = await User.findById(userId)
+               .select("-password_hash")
+               .populate("licenses")
 
-                if (!user) {
-                        return res
-                                .status(404)
-                                .json({ message: "User not found" })
-                }
-                const licenseCount = user.licenses?.length || 0
+          if (!user) {
+               return res.status(404).json({ message: "User not found" })
+          }
+          const licenseCount = user.licenses?.length || 0
 
-                return res.json({
-                        message: "User details successfully retrieved",
-                        user,
-                        licenseCount,
-                })
-        } catch (error) {
-                return res.status(500).json({
-                        message: "Failed to fetch user and licenses",
-                        error: (error as Error).message,
-                })
-        }
+          return res.json({
+               message: "User details successfully retrieved",
+               user,
+               licenseCount,
+          })
+     } catch (error) {
+          return res.status(500).json({
+               message: "Failed to fetch user and licenses",
+               error: (error as Error).message,
+          })
+     }
 }
--- apps/server/src/routes/auth.routes.ts
+++ apps/server/src/routes/auth.routes.ts
@@ -1,16 +1,10 @@
 import { Router } from "express"
-import {
-        signup,
-        login,
-        getAllUsers,
-        editUser,
-} from "../controllers/auth.controller"
+import { signup, login, getAllUsers } from "../controllers/auth.controller"
 import { auth } from "../middleware/jwt"
 
 const router = Router()
 router.post("/signup", signup)
 router.post("/login", login)
 router.get("/users", auth, getAllUsers)
-router.patch("/users/:id", auth, editUser)
 
 export default router
